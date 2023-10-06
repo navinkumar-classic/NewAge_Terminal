6d5fc28c446867b3838172854b1bfbf2f46093f2
Navin_Kumar <121486045+navinkumar-classic@users.noreply.github.com>
2023-10-06 06:59:26 +0530

    Update Hash.py

--- Hash.py
+++ Hash.py
@@ -1,8 +1,51 @@
 from data_structures import HashMap
+import os
 
 hash_database = HashMap(50)
+alias_arr = []
+alias_hash = HashMap(50)
 
 def hello(command):
     return "hi"
 
-hash_database.set("hello",hello)
+def cd(command):
+    folder = command[3:]
+    try:
+        os.chdir(os.getcwd() + "\\" + folder)
+    except:
+        return "File does not exist"
+    else:
+        return "Operation success"
+    
+def mcd(command):
+    folder_list = list(command.split())[1:]
+    try:
+        for folder in folder_list:
+            os.chdir(os.getcwd() + "\\" + folder)
+    except:
+        return "File does not exist"
+    else:
+        return "Operation success"
+
+def alias(command):
+    sub_cmd = list(command.split())
+    if len(sub_cmd) == 1:
+        string = "Alias List\n"
+        for i in alias_arr:
+            act_cmd,bool = alias_hash.get(i)
+            string = string + "\nalias " + i + " - " + act_cmd
+
+        return string
+    
+    else:
+        alias_arr.append(sub_cmd[1])
+        alias_hash.set(sub_cmd[1],' '.join(sub_cmd[2:]))
+
+        return "Alias set"
+        
+def main():
+    hash_set = [["hello",hello],["cd",cd],["mcd",mcd],["alias",alias]]
+    for hash  in hash_set:
+        hash_database.set(hash[0],hash[1])
+
+main()
