9ac66878ff1c271eba77373dc832f96e4e3de54f
Navin_Kumar <121486045+navinkumar-classic@users.noreply.github.com>
2023-10-06 06:53:11 +0530

    Update back.py

--- back.py
+++ back.py
@@ -4,13 +4,25 @@ from Hash import *
 
 def get_fcmd(command):
     l = list(command.split())
-    return l[0]
+    if len(l) > 0: return l[0]
+    else: return ""
+
+def find_alias(command):
+    for i in alias_arr:
+        bool_x = re.search(i, command)
+        if bool_x:
+            command = command.replace(i,alias_hash.get(i)[0])
+            break
+    
+    return command
+    
 # Run the command and capture the output
 def run_cmd(command):
-
-    func,bool = hash_database.get(get_fcmd(command))
     
-    if bool: 
+    command = find_alias(command)
+    func,bool_2 = hash_database.get(get_fcmd(command))
+    
+    if bool_2: 
         return func(command)
     else:
         result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -25,3 +37,9 @@ def run_cmd(command):
 def clean_cmd(command):
     x = re.findall("\$.+", command)
     return x[0][1:].strip()
+
+def get_shrt_dir(dir):
+    dir_list = dir.split('\\')
+    #return dir_list[0] + "\\...\\" + dir_list[-1]
+    return dir_list[-1]
+    
